714d8cdbdcbd3bef022920d10447ba956ae068a0
Joel <pratt.joel@gmail.com>
2016-05-03 15:40:58 -0400

    Pre-project submission

---
+++ imagePickerPractice/imagePickerPractice/MemeStruct.swift
@@ -0,0 +1,18 @@
+//
+//  MemeStruct.swift
+//  imagePickerPractice
+//
+//  Created by Joel on 4/23/16.
+//  Copyright © 2016 Joel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct Meme {
+    var memeTopText : String?
+    var memeBottomText: String?
+    var image: UIImage?
+    var memedImage: UIImage!
+}
